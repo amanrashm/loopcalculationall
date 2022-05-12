8bc1a8ddd16df6953cac88063eb10f06e10519bb
amanrashm <amanra664@gmail.com>
2022-05-12 22:26:33 +0530

    Initial commit

---
+++ src/com/MYCODES/whileloop.java
@@ -0,0 +1,16 @@
+package com.MYCODES;
+
+public class whileloop {
+
+    public static void main(String[] args) {
+	// write your code here
+int i = 0;
+while(i<100){
+
+    System.out.println(i);
+    i+=1;
+
+}
+
+    }
+}
